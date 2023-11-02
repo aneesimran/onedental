fb04364c212bc4c1de8a4f3e9ecfea62317e2599
Anees Imran <anees.imran@sainsburys.co.uk>
2023-11-02 16:47:04 +0000

    worked on navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -2,145 +2,131 @@ import React from "react";
 
 const NavigationBar = () => {
     return (
-        <nav className="bg-theme-dark-blue">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="#" className="flex items-center">
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-                        onedental
-                    </span>
-                </a>
-                <button
-                    data-collapse-toggle="navbar-dropdown"
-                    type="button"
-                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-theme-med-blue focus:outline-none focus:ring-2 focus:ring-theme-light-blue"
-                    aria-controls="navbar-dropdown"
-                    aria-expanded="false"
-                >
-                    <span className="sr-only">Open main menu</span>
-                    <svg
-                        className="w-5 h-5"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 17 14"
-                    >
-                        <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15"
-                        />
-                    </svg>
-                </button>
-                <div
-                    className="hidden w-full md:block md:w-auto"
-                    id="navbar-dropdown"
-                >
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-300 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-theme-dark-blue">
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-theme-light-blue md:p-0 "
-                                aria-current="page"
+        <>
+            <div className="navbar bg-theme-light text-theme-dark-blue">
+                <div className="navbar-start xl:pl-9">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost xl:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="inline-block w-5 h-5 stroke-current"
                             >
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <button
-                                id="dropdownNavbarLink"
-                                data-dropdown-toggle="dropdownNavbar"
-                                className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto"
-                            >
-                                Dropdown{" "}
-                                <svg
-                                    className="w-2.5 h-2.5 ml-2.5"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 10 6"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="m1 1 4 4 4-4"
-                                    />
-                                </svg>
-                            </button>
-                            <div
-                                id="dropdownNavbar"
-                                className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
-                            >
-                                <ul
-                                    className="py-2 text-sm text-gray-700 dark:text-gray-400"
-                                    aria-labelledby="dropdownLargeButton"
-                                >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                ></path>
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 rounded-box w-60"
+                        >
+                            <li>
+                                <a>About us</a>
+                            </li>
+                            <li>
+                                <a>Treatments</a>
+                                <ul className="p-2">
                                     <li>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 hover:bg-gray-100 "
-                                        >
-                                            Dashboard
-                                        </a>
+                                        <a>Fresh Breath Service</a>
                                     </li>
                                     <li>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 hover:bg-gray-100"
-                                        >
-                                            Settings
-                                        </a>
+                                        <a>Teeth Whitening</a>
                                     </li>
                                     <li>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 hover:bg-gray-100"
-                                        >
-                                            Earnings
-                                        </a>
+                                        <a>Invisalign</a>
+                                    </li>
+                                    <li>
+                                        <a>Children</a>
+                                    </li>
+                                    <li>
+                                        <a>Dental Shop</a>
                                     </li>
                                 </ul>
-                                <div className="py-1">
-                                    <a
-                                        href="#"
-                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600"
-                                    >
-                                        Sign out
-                                    </a>
-                                </div>
-                            </div>
+                            </li>
+                            <li>
+                                <a>Fees</a>
+                            </li>
+                            <li>
+                                <a>Testimonials</a>
+                            </li>
+                            <li>
+                                <a>Advice</a>
+                                <ul className="p-2">
+                                    <li>
+                                        <a>Links</a>
+                                    </li>
+                                    <li>
+                                        <a>Privacy Policy</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a>Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl">
+                        onedental
+                    </a>
+                </div>
+                <div className="navbar-end hidden xl:flex pr-10">
+                    <ul className="menu menu-horizontal px-1">
+                        <li>
+                            <a>About us</a>
+                        </li>
+                        <li tabIndex={0}>
+                            <details>
+                                <summary>Treatments</summary>
+                                <ul className="p-2 w-48">
+                                    <li>
+                                        <a>Fresh Breath Service</a>
+                                    </li>
+                                    <li>
+                                        <a>Teeth Whitening</a>
+                                    </li>
+                                    <li>
+                                        <a>Invisalign</a>
+                                    </li>
+                                    <li>
+                                        <a>Children</a>
+                                    </li>
+                                    <li>
+                                        <a>Dental Shop</a>
+                                    </li>
+                                </ul>
+                            </details>
                         </li>
                         <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ay-700"
-                            >
-                                Services
-                            </a>
+                            <a>Fees</a>
                         </li>
                         <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                            >
-                                Pricing
-                            </a>
+                            <a>Testimonials</a>
+                        </li>
+                        <li tabIndex={0}>
+                            <details>
+                                <summary>Advice</summary>
+                                <ul className="p-2 w-48">
+                                    <li>
+                                        <a>Fees</a>
+                                    </li>
+                                    <li>
+                                        <a>Testimonials</a>
+                                    </li>
+                                </ul>
+                            </details>
                         </li>
                         <li>
-                            <a
-                                href="#"
-                                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                            >
-                                Contact
-                            </a>
+                            <a>Contact Us</a>
                         </li>
                     </ul>
                 </div>
             </div>
-        </nav>
+        </>
     );
 };
 
